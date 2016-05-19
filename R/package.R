48bfcf95f59c60c22ee85a7bbb610c4433b0457c
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-19 09:59:44 +0100

    Fix up Mason bugs

--- R/package.R
+++ R/package.R
@@ -2,7 +2,7 @@
 #' Parse Data of R Code as an 'XML' Tree
 #'
 #' Convert the output of 'utils::getParseData()' to an 'XML' tree, that is
-  searchable and easier to manipulate in general.
+#' searchable and easier to manipulate in general.
 #'
 #' @docType package
 #' @name xmlparsedata
