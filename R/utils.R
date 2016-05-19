d709110f780da106b94f8898c7ed686f47bcdaa1
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-19 17:59:37 +0100

    Another micro-optimization

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,6 @@
 
-spaces <- function(x) paste(rep(" ", x), collapse = "")
+spaces_const <- sapply(1:20, function(x) paste(rep(" ", x), collapse = ""))
+
+spaces <- function(x) {
+  if (x > 20) paste(rep(" ", x), collapse = "") else spaces_const[x]
+}
