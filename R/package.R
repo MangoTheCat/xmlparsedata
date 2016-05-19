25a354ddf3f3cbaf4f30bb900fffa44fd7396afe
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-19 23:31:13 +0100

    Vectorized solution, based on positions only
    This one is fast, but is it reliable?

--- R/package.R
+++ R/package.R
@@ -25,103 +25,50 @@ NULL
 xml_parse_data <- function(x, includeText = NA) {
   pd <- getParseData(x, includeText = includeText)
   pd <- fix_comments(pd)
-  pd <- correct_order(pd)
-  pd <- rewrite_ids(pd)
-  forest <- make_forest(pd)
 
-  ## Order, so that id corresponds to line number, simplifies indexing
-  pd <- pd[ order(pd$id), ]
-
-  ## It is easier to do it here, than individually
-  pd$line1 <- as.character(pd$line1)
-  pd$col1  <- as.character(pd$col1)
-  pd$line2 <- as.character(pd$line2)
-  pd$col2 <- as.character(pd$col2)
-
-  outstream <- rawConnection(raw(0), "w")
-  on.exit(close(outstream))
-
-  writeChar(
-    con = outstream, eos = NULL,
-    "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>\n<exprlist>\n"
+  ## Tags for all nodes, teminal nodes have end tags as well
+  pd$token <- map_token(pd$token)
+  pd$tag <- paste0(
+    ifelse(pd$terminal, "  ", ""),
+    "<", pd$token,
+    " line1=\"", pd$line1,
+    "\" col1=\"", pd$col1,
+    "\" line2=\"", pd$line2,
+    "\" col2=\"", pd$col2,
+    "\">",
+    xml_encode(pd$text),
+    ifelse(pd$terminal, paste0("</", pd$token, ">"), "")
   )
 
-  for (c in which(pd$parent == 0)) {
-    to_xml_subtree(pd, forest, c, outstream)
-    writeChar(con = outstream, "\n", eos = NULL)
-  }
+  ## Add an extra terminal tag for each non-terminal one
+  pd2 <- pd[! pd$terminal, ]
+  pd2$terminal <- TRUE
+  pd2$text <- "foo"
+  pd2$line1 <- pd2$line2
+  pd2$col1 <- pd2$col2
+  pd2$line2 <- pd2$line2 - 1L
+  pd2$col2 <- pd2$col2 - 1L
+  pd2$tag <- paste0("</", pd2$token, ">")
 
-  writeChar(con = outstream, "</exprlist>\n", eos = NULL)
+  pd <- rbind(pd, pd2)
 
-  rawToChar(rawConnectionValue(outstream))
+  ## Order the nodes properly
+  pd <- pd[ order(pd$line1, pd$col1, -pd$line2, -pd$col2, pd$terminal), ]
+
+  paste0(
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>\n<exprlist>\n",
+    paste(pd$tag, collapse = "\n"),
+    "\n</exprlist>\n"
+  )
 }
 
 fix_comments <- function(pd) {
   pd[ pd$parent >= 0, ]
 }
 
-## Rewrite the node IDs, so that they are consecutive, and
-## we don't have to access the rows via names. Names are really
-## slow for some reason
-
-rewrite_ids <- function(pd) {
-  levels <- sort(unique(c(pd$id, pd$parent)))
-  pd$id <- as.integer(factor(pd$id, levels = levels)) - 1L
-  pd$parent <- as.integer(factor(pd$parent, levels = levels)) -1L
-  rownames(pd) <- NULL
-  pd
-}
-
-## This is just the order within the file,
-## we don't care about the tree structure here.
-## The purpose is to order the children of a node reasonably well
-
-correct_order <- function(pd) {
-  pd[ order(pd$line1, pd$col1, -pd$line2, -pd$col2), ]
-}
-
-make_forest <- function(pd) {
-  forest <- tapply(pd$id, pd$parent, c, simplify = FALSE)
-  empty <- structure(
-    vector(nrow(pd), mode = "list"),
-    names = 1:nrow(pd)
-  )
-  empty[as.integer(names(forest[-1]))] <- forest[-1]
-  empty
-}
-
-to_xml_subtree <- function(pd, forest, root, outstream, indent = 0) {
-
-  token <- map_token(pd$token[root])
-  text <- xml_encode(pd$text[root])
-  if (text == "") text <- character()
-
-  writeChar(
-    con = outstream, eos = NULL,
-    c(spaces(indent), "<", token,
-      " line1=\"", pd$line1[root],
-      "\" col1=\"", pd$col1[root],
-      "\" line2=\"", pd$line2[root],
-      "\" col2=\"", pd$col2[root],
-      "\">",
-      text,
-      if (length(forest[[root]])) "\n")
-  )
-
-  for (c in forest[[root]]) {
-    to_xml_subtree(pd, forest, c, outstream, indent + 2)
-    writeChar(con = outstream, "\n", eos = NULL)
-  }
-
-  writeChar(
-    con = outstream, eos = NULL,
-    c(if (length(forest[[root]])) spaces(indent), "</", token, ">")
-  )
-}
-
 map_token <- function(token) {
   map <- map[token]
-  if (is.na(map)) token else map
+  ifelse(is.na(map), token, map)
 }
 
 map <- c(
